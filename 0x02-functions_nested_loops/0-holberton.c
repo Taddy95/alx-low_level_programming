a281ded24294c12f39f163e6a324b1f59130d14e
Oghenefejiro Eva Gabje <whitneytaddyeva@gmail.com>
2021-06-22 13:11:11 -0700

    A program that prints Holberton followed by a new line.

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -8,15 +8,13 @@
 int main(void)
 {
 	char c[10] = "Holberton";
-	
 	int i = 0;
-	
-	while (i < 9 )
 
-	 {
-		 _putchar(c[i]);
-		 i++;
-	 }
-	 _putchar('\n');
-	 return (0);
+	while (i < 9)
+	{
+		_putchar(c[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
 }
