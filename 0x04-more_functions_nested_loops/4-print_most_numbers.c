8955115e333e0e111b38d6f775c287f93028ebca
Oghenefejiro Eva Gabje <whitneytaddyeva@gmail.com>
2021-06-24 08:15:06 -0700

    A function that prints the numbers, from 0 to 9, followed by a new lineexcept 2 and 3

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * print_most_numbers - Entry point
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		if (i == 2 || i == 4)
+		{
+			i++;
+			continue;
+		}
+		_putchar(i + '0');
+		i++;
+	}
+	_putchar('\n');
+}
