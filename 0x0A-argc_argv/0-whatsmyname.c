5284237bc1b706d5f0434e938759f1e5058dd55c
Taddy95 <whitneytaddyeva@gmail.com>
2021-07-12 06:32:48 -0700

    Writeprogram that prints its name, followed by a new line.

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry level
+ * @argc: Argument count
+ * @argv: Array of argument strings
+ *
+ * Return: Always 0 (success).
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
