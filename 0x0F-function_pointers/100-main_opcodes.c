db6eff4269a51190254daa829fbbdd8a59be27ee
Taddy95 <whitneytaddyeva@gmail.com>
2021-07-21 05:06:20 -0700

    Write a program that prints the opcodes of its own main function.

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_opcodes - entry point
+ * @a: Address
+ * @n: Number of byte
+ * Return: void;
+ */
+void print_opcodes(char *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%.2hhx", a[i]);
+		if (i < n - 1)
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
+}
+
+/**
+ * main - Etry point
+ * @argc: number of arguments passed
+ *  @argv: array of pointers to arguments
+ *  Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int n;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	n = atoi(argv[1]);
+	if (n < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	print_opcodes((char *)&main, n);
+	return (0);
+}
