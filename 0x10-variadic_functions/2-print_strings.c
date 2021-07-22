d47c3e645c024d8b10bf9223f2f712317a75aed0
Taddy95 <whitneytaddyeva@gmail.com>
2021-07-22 16:52:12 -0700

    Write a function that prints strings, followed by a new line

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -12,8 +12,8 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_list my_list;
 	unsigned int i;
 	char *c;
-	va_start(my_list, n);
 
+	va_start(my_list, n);
 	for (i = 0; i < n; i++)
 	{
 		c = va_arg(my_list, char*);
