f47bae2d3e257708b70a1f763c74790ddd731995
Taddy95 <whitneytaddyeva@gmail.com>
2021-07-29 15:42:18 -0700

    Write a functio that prints all the elements of list_t list

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,30 @@
+#ifndef LISTS_H_
+#define LISTS_H_
+
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * strujct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description:nsingly linked list node structure
+ * for holberton project
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+int _putchar(char);
+size_t print_list(const list_t *h);
+size_t list_ln(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+
+#endif /* _LISTS_H_ */
